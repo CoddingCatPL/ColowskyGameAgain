34238587c54aca0eabd8e182c99f5ebd47706bbd
CoddingCatPL <150941284+CoddingCatPL@users.noreply.github.com>
2025-08-13 02:23:14 +0200

    gagagaragaga

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    CLIENT_ID: "1404563751868436571", // Discord Application ID
+    COMPANY_NAME: "CoddingCat", // Company or Developer Name
+    PRODUCT_NAME: "Smieszne", // Game Name
+    PRODUCT_VERSION: "0.1.0", // Game Version
+};
